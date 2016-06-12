2d14bb56bdcc4cff76d1f302830b9294d39f2047
Kejia Wu <w.kejia@gmail.com>
2016-06-12 00:02:48 -0400

    updating project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject default "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject clogra "0.0.1-SNAPSHOT"
+  :description "a graph algorithm lib for clojure."
+  :url "http://github.com/discretewater/clogra"
+  :license {:name "GNU GENERAL PUBLIC LICENSE"
+            :url "http://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]])
